65104c1226a0549d45970e7322c7a4fec2134f91
icacedo <55859921+icacedo@users.noreply.github.com>
2019-11-15 16:18:58 -0800

    Create 01_rotation_notes.sh

---
+++ 01_rotation_notes.sh
@@ -0,0 +1,8 @@
+###### Using the Farm cluster ######
+## use history to view everything you previously input
+  history
+## always activate the appropriate conda environment
+## log in and provide your private key
+  ssh -i ~/ssh/2019-11-15-farm icacedo@farm.cse.ucdavis.edu
+## use this node to download files
+  ssh -p 2022 -i ~/ssh/2019-11-15-farm icacedo@farm.cse.ucdavis.edu
