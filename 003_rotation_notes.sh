0e3adadc8230f9b56219e2c2bb0c94b2670b3b14
icacedo <55859921+icacedo@users.noreply.github.com>
2019-11-25 22:27:34 -0800

    Create 003_rotation_notes.sh

---
+++ 003_rotation_notes.sh
@@ -0,0 +1,6 @@
+## November 25, 2019
+## for random subsampling, use khmer: https://anaconda.org/bioconda/khmer
+## use this script specifically (view on github): khmer/scripts/sample-reads-randomly.py
+## created new directory within test_reads/ called random/
+## install khmer in F19_diblab_rotation (~/)
+  conda install -c bioconda khmer
