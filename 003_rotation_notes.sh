2a8872f2b3bc7a1e6fb8bedf27b87fe1253553eb
icacedo <55859921+icacedo@users.noreply.github.com>
2019-11-25 23:35:36 -0800

    Update 003_rotation_notes.sh

--- 003_rotation_notes.sh
+++ 003_rotation_notes.sh
@@ -4,4 +4,11 @@
 ## created new directory within test_reads/ called random/
 ## install khmer in F19_diblab_rotation (~/)
   conda install -c bioconda khmer
-## run khmer to subset data randomly
+## install jupyter lab on your farm account (F19_diblab_rotation environment)
+  conda install -c conda-forge jupyterlab
+## could not get jupyterlab to connect through my browser
+## installed regular jupyter instead
+  conda install -c conda-forge jupyter
+## nothing was different, so i removed jupyter
+  conda remove -c conda-forge jupyter
+## it probably won't work because i'm on farm, and i'm trying to open the notebook on my desktop
