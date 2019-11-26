48f83c632f616343a6034f508c94e9f4ec2a90c6
icacedo <55859921+icacedo@users.noreply.github.com>
2019-11-26 11:56:50 -0800

    Update 003_rotation_notes.sh

--- 003_rotation_notes.sh
+++ 003_rotation_notes.sh
@@ -1,7 +1,4 @@
 ## November 25, 2019
-## for random subsampling, use khmer: https://anaconda.org/bioconda/khmer
-## use this script specifically (view on github): khmer/scripts/sample-reads-randomly.py
-## created new directory within test_reads/ called random/
 ## install khmer in F19_diblab_rotation (~/)
   conda install -c bioconda khmer
 ## install jupyter lab on your farm account (F19_diblab_rotation environment)
@@ -12,3 +9,5 @@
 ## nothing was different, so i removed jupyter
   conda remove -c conda-forge jupyter
 ## it probably won't work because i'm on farm, and i'm trying to open the notebook on my desktop
+###########################################################################################################################
+## November 26, 2019
