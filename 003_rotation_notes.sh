e6c4906631d88b9df5ac2ddd5abffb9fd6718070
icacedo <55859921+icacedo@users.noreply.github.com>
2019-11-28 22:11:53 -0800

    Update 003_rotation_notes.sh

--- 003_rotation_notes.sh
+++ 003_rotation_notes.sh
@@ -29,6 +29,23 @@
   screen 
   srun -p bmm -J tmp -t 72:00:00 --mem=2000 --pty bash
   ./F19_diblab_rotation/scripts/run_khmer.sh
-
+## run plass
+## https://lifebit.gitbook.io/cloudos/pipelines-documentations-and-examples-1/docker-pipelines-and-tools/plass
+## made new directory ~/F19_diblab_rotation/plass_assembly/
+## made script to run plass ~/F19_diblab_rotation/scripts/run_plass.sh
+  #!/bin/bash
+  for file in ~/F19_diblab_rotation/test_reads/random/*R1*
+  do
+          file1=$file
+          file2=`echo $file1 | awk -F"R1" '{print $1 "R2" $2}'`
+          base=`echo $file1 | sed -e 's/_R1//' | sed -e 's/.fastq.gz//'`
+          plass assemble $file1 $file2 ~/F19_diblab_rotation/plass_assembly/$base.assembly.fas tmp
+  done
+## run plass 
+  screen
+  srun -p bmm -J tmp -t 72:00:00 --mem=2000 --pty bash
+  conda activate F19_diblab_rotation
+  chmod 755 ./run_plass.sh
+  ./run_plass.sh
 
   
